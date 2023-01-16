9043d3a8daa2ee592a7412b9ba3e46255c45d698
Arttu Pennanen <arttu@pennanen.org>
2023-01-16 23:12:18 +0200

    rm misc file

--- :!node 03.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-import * as R from 'ramda'
-
-const FILE_NAME = "01.input"
-
-const readFile = n => fs.readFileSync(n, 'utf8')
-const FILE = readFile(FILE_NAME)
-console.log(FILE)
