b9b34e213fa0506ddc45ca222c7adc558b208ba4
Arttu Pennanen <arttu@pennanen.org>
2023-01-17 02:12:52 +0200

    5 part 2 done

--- 05.js
+++ 05.js
@@ -1,9 +1,9 @@
 import fs from 'fs'
 import * as R from 'ramda'
 
-const FILE_NAME = "05.input"
+const FILE_NAME = '05.input'
 
-const readFile = n => fs.readFileSync(n, 'utf8')
+const readFile = (n) => fs.readFileSync(n, 'utf8')
 const FILE = readFile(FILE_NAME)
 
 const headLens = R.lensProp(0)
@@ -13,46 +13,61 @@ const viewHead = R.view(headLens)
 const viewLast = R.view(lastLens)
 
 const parseStacks = R.pipe(
-    R.pipe(viewHead, R.init),
-    R.map(R.splitEvery(4)),
-    R.transpose,
-    R.map(R.filter(R.test(/\w/g))),
-    R.map(R.chain(R.match(/\w/g))),
+  R.pipe(viewHead, R.init),
+  R.map(R.splitEvery(4)),
+  R.transpose,
+  R.map(R.filter(R.test(/\w/g))),
+  R.map(R.chain(R.match(/\w/g)))
 )
 
 const parseMoves = R.pipe(
-    R.pipe(viewLast, R.tail, R.init),
-    R.map(R.pipe(
-        R.slice("move ".length, Infinity),
-        R.split(" from "),
-        R.chain(R.split(" to ")),
-        R.map(parseInt)
-    )),
+  R.pipe(viewLast, R.tail, R.init),
+  R.map(
+    R.pipe(
+      R.slice('move '.length, Infinity),
+      R.split(' from '),
+      R.chain(R.split(' to ')),
+      R.map(parseInt)
+    )
+  )
 )
 
 const parseStacksAndMoves = R.juxt([parseStacks, parseMoves])
 
 const parseFile = R.pipe(
-    R.split('\n'),
-    R.splitWhen(R.isEmpty),
-    parseStacksAndMoves
+  R.split('\n'),
+  R.splitWhen(R.isEmpty),
+  parseStacksAndMoves
 )
 
-const applyMove = (stacks, [count, from, target]) => {
+const applyMove =
+  (multiple) =>
+  (stacks, [count, from, target]) => {
     const [moving, newFrom] = R.splitAt(count, stacks[from - 1])
-    const newTarget = moving.reverse().concat(stacks[target - 1])
-    
+
+    const newTarget = R.pipe(
+      R.unless(R.always(multiple), R.reverse),
+      R.flip(R.concat)(stacks[target - 1])
+    )(moving)
+
     return R.pipe(
-        R.update(from - 1, newFrom),
-        R.update(target - 1, newTarget)
+      R.update(from - 1, newFrom),
+      R.update(target - 1, newTarget)
     )(stacks)
-}
+  }
 
 const pipeline = R.pipe(
-    parseFile,
-    R.apply(R.reduce(applyMove)),
-    R.map(R.head),
-    R.join("")
+  parseFile,
+  R.apply(R.reduce(applyMove(false))),
+  R.map(R.head),
+  R.join('')
 )
 
-console.log(pipeline(FILE))
+const pipeline2 = R.pipe(
+  parseFile,
+  R.apply(R.reduce(applyMove(true))),
+  R.map(R.head),
+  R.join('')
+)
+
+console.log(pipeline(FILE), pipeline2(FILE))
