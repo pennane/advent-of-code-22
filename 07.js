c86fbbec0d89cd14eb99a33a793bab925c322173
Arttu Pennanen <arttu@pennanen.org>
2023-01-17 16:15:20 +0200

    7 part 1 done

---
+++ 07.js
@@ -0,0 +1,130 @@
+import fs from 'fs'
+import * as R from 'ramda'
+
+const FILE_NAME = '07.input'
+
+const readFile = (n) => fs.readFileSync(n, 'utf8')
+
+const FILE = readFile(FILE_NAME)
+
+const parseFile = R.pipe(
+  R.split('$ '),
+  R.map(R.pipe(R.split('\n'), R.chain(R.when(R.isEmpty, R.always([]))))),
+  R.reject(R.isEmpty)
+)
+
+const currentPath = R.nth(1)
+const currentCommand = R.nth(0)
+const commandName = R.pipe(currentCommand, R.nth(0), R.split(' '), R.nth(0))
+const argName = R.pipe(currentCommand, R.nth(0), R.split(' '), R.nth(1))
+const commandIs = (name) => R.pipe(commandName, R.equals(name))
+const argIs = (name) => R.pipe(argName, R.equals(name))
+
+const flipTuple = ([a, b]) => [b, a]
+
+const parseFiles = R.pipe(
+  R.tail,
+  R.map(R.split(' ')),
+  R.reject(R.pipe(R.head, R.equals('dir'))),
+  R.map(R.pipe(flipTuple, R.adjust(1, parseInt))),
+  R.fromPairs
+)
+
+const parseNewPath = R.cond([
+  [argIs('/'), R.always([])],
+  [argIs('..'), R.pipe(currentPath, R.init)],
+  [R.T, R.converge(R.append, [argName, currentPath])]
+])
+
+const parseCommand = R.cond([
+  [
+    commandIs('ls'),
+    R.pipe(([command, path, dirs]) => [
+      path,
+      R.over(R.lensPath(path), R.mergeDeepLeft(parseFiles(command)), dirs)
+    ])
+  ],
+  [
+    commandIs('cd'),
+    R.pipe(([command, path, dirs]) =>
+      R.pipe(
+        parseNewPath,
+        R.juxt([
+          R.identity,
+          (path) =>
+            R.over(R.lensPath(path), R.when(R.isNil, R.always({})), dirs)
+        ])
+      )([command, path])
+    )
+  ],
+  [R.T, ([_, path, dirs]) => [path, dirs]]
+])
+
+const directorySize = R.unless(
+  R.is(Number),
+  R.pipe(
+    R.values,
+    R.reduce((sum, val) => {
+      if (R.is(Number, val)) {
+        return sum + val
+      }
+      return directorySize(val) + sum
+    }, 0)
+  )
+)
+
+
+const directorySizeFromPath = (dirs) => (path) => R.pipe(
+    R.path(path),
+    directorySize
+)(dirs)
+
+
+const isObject = val => {
+    if (val === null) { return false;}
+    return ( (typeof val === 'function') || (typeof val === 'object') );
+}
+
+const allDirectoryPaths = (val, currentPath = []) => {
+    const entries = Object.entries(val)
+    const low = entries.flatMap(([key, value]) => isObject(value) 
+        ? [currentPath.concat(key)]
+        : []
+    )
+
+    const deep = entries.flatMap(([key, value]) => isObject(value) 
+        ? allDirectoryPaths(value, currentPath.concat(key))
+        : []
+    )
+
+    return low.concat(deep)
+}
+
+
+
+const parseDirectoryTree = R.pipe(
+  R.reduce(
+    ([path, dirs], command) => {
+      const parsed = parseCommand([command, path, dirs])
+      return parsed
+    },
+    [[], []]
+  ),
+  R.nth(1)
+)
+
+const asdf = (file) => {
+    const parsedFile = parseFile(file)
+    const dirs = parseDirectoryTree(parsedFile)
+    const paths = R.uniq(allDirectoryPaths(dirs))
+    const dirSizes = paths.map(directorySizeFromPath(dirs))
+    const lt100000 = dirSizes.filter(n => n <= 100000)
+    fs.writeFileSync('dirs.json', JSON.stringify(dirs))
+    fs.writeFileSync('paths.json', JSON.stringify(paths))
+    fs.writeFileSync('dirSizes.json', JSON.stringify(dirSizes))
+    fs.writeFileSync('lt100.json', JSON.stringify(lt100000))
+    return R.sum(lt100000)
+}
+
+
+console.log(asdf(FILE))
